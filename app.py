5a4dfee4e80f5da9b4d00b5e45427902b1bcd974
john681611 <10814889+john681611@users.noreply.github.com>
2020-06-10 15:04:04 +0100

    pressess bids more effectively

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 import database
 import operator
 
+def get_max_bid(bids): 
+    return max([x['amount'] for x in bids])
+
 def process_sell(sell):
     database.add(sell['item'], sell)
 
+
 def proccess_bid(bid):
     item = database.get(bid['item'])
-    if item['bids'] != [] and max([x['amount'] for x in item['bids']]) > bid['bid_amount']:
-        print('in')
+    if item == None:
+        print(f"No item {bid['item']} found for sale")
         return
+    elif item['close_time'] < bid['timestamp']:
+        print(f"Bid too late closed at {item['close_time']}")
+        return
+    elif len(item['bids']) > 0 and get_max_bid(item['bids']) > bid['bid_amount']:
+        print(f"Bid too low max is {get_max_bid(item['bids'])}")
+        return
+    
     bid_details = {
         'user_id': bid['user_id'],
         'amount': bid['bid_amount'],
@@ -16,3 +27,4 @@ def proccess_bid(bid):
     }
     item['bids'].append(bid_details)
     database.add(item['item'],item)
+    return bid_details
--- test_app.py
+++ test_app.py
@@ -16,7 +16,7 @@ def test_process_bid_first_bid():
     sell = {'close_time': 20,'item': 'toaster_2','reserve_price': 10.0,'timestamp': 10, 'user_id': 1, 'bids': []}
     app.process_sell(sell)
     bid = {'bid_amount': 7.5, 'item': 'toaster_2', 'timestamp': 12, 'user_id': 8}
-    app.proccess_bid(bid)
+    assert app.proccess_bid(bid) == {'user_id': 8, 'amount': 7.5, 'timestamp': 12}
     assert database.get('toaster_2')['bids'] == [{
         'user_id': 8,
         'amount': 7.5,
@@ -29,7 +29,7 @@ def test_process_bid_second_bid():
     ]}
     app.process_sell(sell)
     bid = {'bid_amount': 8, 'item': 'toaster_3', 'timestamp': 13, 'user_id': 9}
-    app.proccess_bid(bid)
+    assert app.proccess_bid(bid) == {'user_id': 9, 'amount': 8, 'timestamp': 13}
     assert database.get('toaster_3')['bids'] == [
         {'user_id': 8, 'amount': 7.5, 'timestamp': 12},
         {'user_id': 9, 'amount': 8, 'timestamp': 13}
@@ -41,13 +41,18 @@ def test_process_bid_lower_second_bid():
     ]}
     app.process_sell(sell)
     bid = {'bid_amount': 7.5, 'item': 'toaster_4', 'timestamp': 14, 'user_id': 8}
-    app.proccess_bid(bid)
+    assert app.proccess_bid(bid) == None
     assert database.get('toaster_4')['bids'] == [
         {'user_id': 9, 'amount': 8, 'timestamp': 13}
         ]
 
 def test_process_bid_no_sell():
-    pass
+    bid = {'bid_amount': 7.5, 'item': 'toaster_4', 'timestamp': 14, 'user_id': 8}
+    assert app.proccess_bid(bid) == None
 
 def test_process_bid_closed_sell():
-    pass
+    sell = {'close_time': 20,'item': 'toaster_5','reserve_price': 10.0,'timestamp': 10, 'user_id': 1, 'bids': []}
+    app.process_sell(sell)
+    bid = {'bid_amount': 7.5, 'item': 'toaster_5', 'timestamp': 21, 'user_id': 8}
+    assert app.proccess_bid(bid) == None
+    assert database.get('toaster_5')['bids'] == []

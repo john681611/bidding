4189827bb1fe768be02068db983a1ab7b30baf69
john681611 <10814889+john681611@users.noreply.github.com>
2020-06-10 17:17:50 +0100

    write file logic

---
+++ write_file.py
@@ -0,0 +1,7 @@
+
+def write(data, location):
+    with open(location, 'w+') as text_file:
+        text_file.write(data)
+
+def format_data(data): 
+    return f"{data['close_time']}|{data['item']}|{data['user_id']}|{data['status']}|{data['price_paid']:.2f}|{data['total_bid_count']}|{data['highest_bid']:.2f}|{data['lowest_bid']:.2f}\n"
